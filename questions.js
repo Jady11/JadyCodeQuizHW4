e5fee04f32088b0891619fff12be38bbbae9f6d6
Jady Tippetts <jady11@gmail.com>
2020-10-09 13:21:53 -0600

    initial push with HTML, CSS and JS files.  Created HTML page.  Variables in JS and a JS question file.

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['Strings', 'Booleans', 'Alerts', 'Numbers'],
+        answer: 'Alerts'
+    },
+    {
+        title: 'The condition in an if/else statement is enclosed within ______.',
+        choices: ['Quotes', 'Curly Brackets', 'Parentheses', 'Square Brackets'],
+        answer: 'Parentheses'
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store_____.',
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above'
+    },
+    {
+        title: 'String values must be enclosed within _______ when being assigned to variables',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes'
+    },
+    {
+        title: 'A very useful tool used during development and debugging for content',
+        choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+        answer: 'console.log'
+    },
+];
---
+++ script.js
@@ -0,0 +1,13 @@
+// quiz variables
+var questionsIndex = 0;
+var time = questions.length * 10;
+var timerId;
+
+// create variables for DOM
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
